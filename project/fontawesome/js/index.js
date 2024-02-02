52ccb8eec703a69a5945c897bfb53edcb3a6b857
deepakkurmi5 <deepakkurmi874@gmail.com>
2024-02-02 22:43:36 +0530

    completed

--- project/fontawesome/js/index.js
+++ project/fontawesome/js/index.js
@@ -1,16 +1,6 @@
-const menuBar = document.getElementById("bars");
-const closeMenu = document.querySelector(".fa-xmark");
-const mobileMenu = document.getElementById("mobile-menu");
+const mobileIcons = document.querySelector(".mobile-icons");
+const headerSection = document.querySelector(".header-section");
 
-menuBar.addEventListener("click", function () {
-  mobileMenu.style.display = "block";
-  closeMenu.style.display = "block";
-  menuBar.style.display = "none";
-  mobileMenu.classList.toggle("menu-show");
-});
-
-closeMenu.addEventListener("click", function () {
-  mobileMenu.style.display = "none";
-  closeMenu.style.display = "none";
-  menuBar.style.display = "block";
+mobileIcons.addEventListener("click", function () {
+  headerSection.classList.toggle("active");
 });
